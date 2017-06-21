15a35e02a20c1ff9213349d1f8c65919acaa00bf
Fabrício Zimmerer Murta <fabricio@object.net>
2017-06-21 16:10:15 -0300

    Adds the test case for the second issue on forum thread 61968.

--- TestProject/TestProject/Controllers/issuesController.cs
+++ TestProject/TestProject/Controllers/issuesController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Ext.Net;
+using Ext.Net.MVC;
+using TestProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +15,50 @@ namespace TestProject.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public DirectResult SaveData(string id, FormCollection values, string startDate, string endDate)
+        {
+            DirectResult response = new DirectResult();
+
+            Ext.Net.Store store = X.GetCmp<Ext.Net.Store>("StoreTest");
+            StoreDataHandler dataHandler = new StoreDataHandler(values["data"]);
+            ChangeRecords<TestData> data = dataHandler.BatchObjectData<TestData>();
+
+            for (int i = 0; i < data.Updated.Count; i++)
+            {
+                TestData testData = data.Updated[i];
+
+                //we want to remove the record since it is processed so we use the delete record option
+                ModelProxy record = store.GetById(testData.id);
+                store.Remove(record);
+                store.CommitRemoving(testData.id);
+            }
+
+            return response;
+        }
+
+        public ActionResult GetData()
+        {
+            List<TestData> l = new List<TestData>();
+            for (int i = 0; i < 2000; i++)
+            {
+                TestData t = new TestData();
+                t.id = i;
+                t.startTestData = "test_" + i;
+                t.endTestData = "test_" + i;
+                l.Add(t);
+            }
+
+            var query = l.Select(dt => new
+            {
+                id = dt.id,
+                startTestData = dt.startTestData,
+                endTestData = dt.endTestData
+
+            }).OrderBy(d => d.id);
+
+            return this.Store(query);
         }
     }
 }
---
+++ TestProject/TestProject/Models/TestData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProject.Models
+{
+    public class TestData
+    {
+        public long id;
+        public string startTestData;
+        public string endTestData;
+    }
+}
